ec271467c33a9c69ef5d4440985b57a613fa5e98
matheswaaran96 <mat@testpress.in>
2022-01-05 22:30:43 +0530

    - Added Modal popup component

---
+++ src/components/Modals/MainModal.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "./index.css";
+
+const MainModal = (props) => {
+  if (!props.visible) {
+    return null;
+  }
+
+  return (
+    <div className="modal-background-container">
+      <div className="modal-container">
+        <div className="modal-title-header">
+          <span className="modal-title">{props.title}</span>
+          <span className="modal-close" onClick={props.onClose}>
+            {props.closeIcon}
+          </span>
+        </div>
+        <div className="modal-content">{props.children}</div>
+        <div className="modal-actions">
+          {props.renderFooter ? (
+            props.renderFooter()
+          ) : (
+            <div className="modal-default-actions">
+              <button className="modal-actions-ok-btn" onClick={props.onClose}>
+                Ok
+              </button>
+              <button
+                className="modal-actions-cancel-btn"
+                onClick={props.onClose}
+              >
+                Cancel
+              </button>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+MainModal.defaultProps = {
+  title: "",
+  visible: false,
+  closeIcon: <>&times;</>,
+  onClose: () => {},
+  renderFooter: undefined,
+};
+
+export default MainModal;
