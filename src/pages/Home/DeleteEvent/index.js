22162a5091e0541cce563ae62be8fba314f4dd1a
Matheswaaran <matheswaarans@gmail.com>
2022-01-10 03:35:05 +0530

    feat: Added Delete event (#26)

---
+++ src/pages/Home/DeleteEvent/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./index.css";
+import MainModal from "../../../components/Modals/MainModal";
+
+const DeleteModal = (props) => {
+  return (
+    <MainModal
+      title=""
+      visible={props.open}
+      onClose={() => props.setOpen(false)}
+      renderFooter={() => <React.Fragment />}
+    >
+      <div className="delete-modal-container">
+        <span>Are you sure want to delete the event?</span>
+      </div>
+      <div className="delete-actions">
+        <button
+          className="form-actions-delete-btn"
+          onClick={() => props.deleteCalendarEvent()}
+        >
+          Delete
+        </button>
+        <button
+          className="form-actions-cancel-btn"
+          onClick={() => props.setOpen(true)}
+        >
+          Cancel
+        </button>
+      </div>
+    </MainModal>
+  );
+};
+
+export default DeleteModal;
