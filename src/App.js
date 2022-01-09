84e1eb3e04e9475e277f129d06f4c407b9b2c95c
Matheswaaran <matheswaarans@gmail.com>
2022-01-10 02:21:43 +0530

    refactor: Calendar Context into a separate file (#21)

--- src/App.js
+++ src/App.js
@@ -1,57 +1,16 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import CalendarContext from "./context/CalendarContext";
+import { CalendarProvider } from "./context/CalendarContext";
 import Layout from "./Layout";
 
 import Home from "./pages/Home";
 import PageNotFound from "./pages/PageNotFound";
 
-import {
-  addCalendarEventToLocalStorage,
-  getCalendarEventsFromLocalStorage,
-  removeCalendarEventsFromLocalStorage,
-  updateCalendarEventsInLocalStorage,
-} from "./utils";
-
 import "./App.css";
 
 function App() {
-  const [month, setMonth] = React.useState(new Date().getMonth());
-  const [year, setYear] = React.useState(new Date().getFullYear());
-  const [calendarEvents, setCalendarEvents] = React.useState([]);
-
-  React.useEffect(() => {
-    setCalendarEvents(getCalendarEventsFromLocalStorage());
-  }, []);
-
-  const addCalendarEvents = (calendar_event) => {
-    let calendar_events = addCalendarEventToLocalStorage(calendar_event);
-    setCalendarEvents(calendar_events);
-  };
-
-  const updateCalendarEvents = (calendar_event) => {
-    let calendar_events = updateCalendarEventsInLocalStorage(calendar_event);
-    setCalendarEvents(calendar_events);
-  };
-
-  const deleteCalendarEvents = (calendar_event) => {
-    let calendar_events = removeCalendarEventsFromLocalStorage(calendar_event);
-    setCalendarEvents(calendar_events);
-  };
-
   return (
-    <CalendarContext.Provider
-      value={{
-        month,
-        year,
-        setMonth,
-        setYear,
-        calendarEvents,
-        addCalendarEvents,
-        updateCalendarEvents,
-        deleteCalendarEvents,
-      }}
-    >
+    <CalendarProvider>
       <Routes>
         <Route
           path="/"
@@ -63,7 +22,7 @@ function App() {
         />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
-    </CalendarContext.Provider>
+    </CalendarProvider>
   );
 }
 
--- src/context/CalendarContext.js
+++ src/context/CalendarContext.js
@@ -1,5 +1,55 @@
 import React from "react";
+import {
+  addCalendarEventToLocalStorage,
+  getCalendarEventsFromLocalStorage,
+  removeCalendarEventsFromLocalStorage,
+  updateCalendarEventsInLocalStorage,
+} from "../utils";
 
 const CalendarContext = React.createContext();
+
+const CalendarProvider = (props) => {
+  const [month, setMonth] = React.useState(new Date().getMonth());
+  const [year, setYear] = React.useState(new Date().getFullYear());
+  const [calendarEvents, setCalendarEvents] = React.useState([]);
+
+  React.useEffect(() => {
+    setCalendarEvents(getCalendarEventsFromLocalStorage());
+  }, []);
+
+  const addCalendarEvents = (calendar_event) => {
+    let calendar_events = addCalendarEventToLocalStorage(calendar_event);
+    setCalendarEvents(calendar_events);
+  };
+
+  const updateCalendarEvents = (calendar_event) => {
+    let calendar_events = updateCalendarEventsInLocalStorage(calendar_event);
+    setCalendarEvents(calendar_events);
+  };
+
+  const deleteCalendarEvents = (calendar_event) => {
+    let calendar_events = removeCalendarEventsFromLocalStorage(calendar_event);
+    setCalendarEvents(calendar_events);
+  };
+
+  return (
+    <CalendarContext.Provider
+      value={{
+        month,
+        year,
+        setMonth,
+        setYear,
+        calendarEvents,
+        addCalendarEvents,
+        updateCalendarEvents,
+        deleteCalendarEvents,
+      }}
+    >
+      {props.children}
+    </CalendarContext.Provider>
+  );
+};
+
+export { CalendarProvider };
 
 export default CalendarContext;
